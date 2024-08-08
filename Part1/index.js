69ad0fa842a99d2e6693d420d3cf7f7f45c38a60
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-08 15:23:29 -0600

    finished part 1. tie check slightly off solution, but works

--- Part1/index.js
+++ Part1/index.js
@@ -5,7 +5,7 @@ const play = (evt) => {
     const currentPlayer = document.querySelector('#current-player')
     currentPlayer.innerText = player
     evt.target.innerText = player
-    winner()
+    calculateWinner()
 }
 
 const squares = document.querySelectorAll('.square')
@@ -13,23 +13,61 @@ const squares = document.querySelectorAll('.square')
 for (let square of squares) {
     square.addEventListener('click', play)
 }
+// code i came up with for detection of winner. Not great
+// const winner = () => {
+//     for (i = 0; i < squares.length; i += 3) {
+//         if (squares[i].innerText === squares[i+1].innerText && squares[i].innerText === squares[i+2].innerText && squares[i].innerText !== '') {
+//             alert(`Player ${player} is the winner!`)
+//         }
+//     }
+//     for (i = 0; i < 3; i++) {
+//         if (squares[i].innerText === squares[i+3].innerText && squares[i].innerText === squares[i+6].innerText && squares[i].innerText !== '') {
+//             alert(`Player ${player} is the winner!`)
+//         }
+//     }
+//     if (squares[0].innerText === squares[4].innerText && squares[0].innerText === squares[8].innerText && squares[0].innerText !== '') {
+//         alert(`Player ${player} is the winner!`)
+//     }
+//     if (squares[2].innerText === squares[4].innerText && squares[2].innerText === squares[6].innerText && squares[2].innerText !== '') {
+//         alert(`Player ${player} is the winner!`)
+//     }
+// }
 
-const winner = () => {
-    for (i = 0; i < squares.length; i += 3) {
-        if (squares[i].innerText === squares[i+1].innerText && squares[i].innerText === squares[i+2].innerText && squares[i].innerText !== '') {
-            alert(`Player ${player} is the winner!`)
+const calculateWinner = () => {
+    const lines = [
+        // Horizontal Lines
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        // Vertical Lines
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        // Diagonal Lines
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    // for... of loop in order to cycle through these lines
+    for (const line of lines) {
+        const [a, b, c] = line
+        // get the text in each square
+        const squareAText = squares[a].innerText
+        const squareBText = squares[b].innerText
+        const squareCText = squares[c].innerText
+        // If the first square is not blank, and it matches the other squares then we have a winner
+        if (squareAText !== '' && squareAText === squareBText && squareAText === squareCText) {
+            return alert(`Player ${squareAText} is the Winner!`)
         }
     }
-    for (i = 0; i < 3; i++) {
-        if (squares[i].innerText === squares[i+3].innerText && squares[i].innerText === squares[i+6].innerText && squares[i].innerText !== '') {
-            alert(`Player ${player} is the winner!`)
-        }
+    // check if there are no blank innerText portions. If true, return tie game
+    let checkTie = true
+    for (i = 0; i < squares.length; i++) {
+        if (squares[i].innerText === '') {
+            checkTie = false 
+        } 
     }
-    if (squares[0].innerText === squares[4].innerText && squares[0].innerText === squares[8].innerText && squares[0].innerText !== '') {
-        alert(`Player ${player} is the winner!`)
-    }
-    if (squares[2].innerText === squares[4].innerText && squares[2].innerText === squares[6].innerText && squares[2].innerText !== '') {
-        alert(`Player ${player} is the winner!`)
+    if (checkTie) {
+        return alert("Tie Game!")
     }
     
 }
